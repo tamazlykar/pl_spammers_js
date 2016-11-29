1ccf14c762a83fb645cc1198c6e7c5a6d2622f7a
Tamazlykar Daniil <tadani7@gmail.com>
2016-11-29 23:49:06 +0700

    done

---
+++ README.md
@@ -0,0 +1,12 @@
+# Spammers-JS
+
+This app used Promises in execution flow.
+
+<b>index.js</b> - main part of programm;
+<br><b>requestModule.js</b> - send requests and return html;
+<br><b>search.js</b> - find and return array of email's and links from text
+
+<b>Run:</b>
+```
+node index.js
+```
--- index.js
+++ index.js
@@ -3,31 +3,37 @@
 let requestModule = require('./requestModule');
 let searchModule = require('./search');
 
-let host = 'http://www.csd.tsu.ru';
+let host = 'http://www.mosigra.ru';
 
 let awaitingLinks = new Set();
 let passedLinks = new Set();
 let emailsSet = new Set();
 
-let boundary = 10;
+let boundary = 20;
+let counter = 0;
+
+let opinion = true;
 
 function search(text) {
-    searchModule.getEmailList(text)
-        .then(result => {
-            console.log(result);
-            addEmails(result);
-        },
-        error => {
-            console.log(error);
-        });
+    return new Promise((resolve, reject) => {
+        let p1 = searchModule.getEmailList(text)
+            .then(result => {
+                    addEmails(result);
+                },
+                error => {
+                    console.log(error);
+                });
 
-    searchModule.getUrlList(text)
-        .then(result => {
-            addLinks(result);
-        },
-        error => {
-            console.log(error);
-        });
+        let p2 = searchModule.getUrlList(text)
+            .then(result => {
+                    addLinks(result);
+                },
+                error => {
+                    console.log(error);
+                });
+
+        Promise.all([p1, p2]).then(val => resolve());
+    });
 }
 
 
@@ -39,7 +45,7 @@ function addLinks(links) {
     }
 
     awaitingLinks.forEach((val, val2, awaitingLinks) => {
-        console.log(val);
+        //console.log(val);
     })
 }
 
@@ -49,29 +55,59 @@ function addEmails(emails) {
     }
 
     emailsSet.forEach((val, val2, emailsSet) => {
+        //console.log(val);
+    })
+}
+
+function parsePage(url) {
+    console.log('Parsing this url:' + url);
+
+    passedLinks.add(url);
+    requestModule.getHtmlFromUrl(url)
+        .then(result => {
+                search(result).then(res => {
+                    counter++;
+                    decider();
+                    run();
+                });
+            },
+            error => {
+                console.log(error);
+            });
+}
+
+function decider() {
+    if (!(counter < boundary && awaitingLinks.size != 0)) {
+        opinion = false;
+
+        printEmails();
+    }
+}
+
+function run() {
+    if (opinion) {
+        let setIter = awaitingLinks.values();
+        let url = setIter.next().value;
+        awaitingLinks.delete(url);
+
+        parsePage(url);
+    }
+}
+
+function printEmails() {
+    console.log('\n');
+    console.log('Email\'s:');
+    emailsSet.forEach((val, val2, emailsSet) => {
         console.log(val);
     })
 }
 
-
 // Execution start point
-
 
 searchModule.buildUrlPatternRegExp(host);
 
 let myUrl = host;
 
-do {
-    passedLinks.add(myUrl);
-    requestModule.getHtmlFromUrl(myUrl)
-        .then(result => {
-                search(result);
-            },
-            error => {
-                console.log(error);
-            });
+parsePage(myUrl);
 
-    myUrl = awaitingLinks;  // from here
-} while (passedLinks < boundary);
 
-console.log(passedLinks.length);
